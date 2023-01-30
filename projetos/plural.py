2fdc71230d5f09d1d4309138691618bec916af0d
David Freitas <dvdfreitas@gmail.com>
2023-01-30 16:04:50 +0000

    230130 16:00

--- projetos/plural.py
+++ projetos/plural.py
@@ -1,25 +1,56 @@
+lista1 = [
+    ('mão', 'mãos'), 
+    ('sopa', 'sopas'),
+]
+
+lista2 = [
+    ('mão', 'mãos'), 
+    ('aula', 'aulas'),
+]
+
 def calcula_plural(palavra):
+
+    plural = palavra + "s"
 
     return plural
 
-print(calcula_plural(123))
+lista1 = [
+    ('mão', 'mãos'), 
+    ('aula', 'aulas'),
+    ('sopa', 'sopas'),
+]
 
-lista_de_palavras = [('computador', 'computadores'), ('caroça', 'caroças'), ('centímo', 'euros')]
 
-print("Introduza a palavra:", end='')
-palavra = input()
+def junta_listas(lista1, lista2):
 
-# if palavra[len(palavra)-1] == "l" and palavra[len(palavra)-2] == "a":
+    resultado = []
+
+    for element in lista1:
+        if element not in lista2: 
+            resultado += element
+    resultado += lista2
+
+    return resultado
+
+print(junta_listas(lista1, lista2))
+
+
+# lista_de_palavras = [('computador', 'computadores'), ('caroça', 'caroças'), ('centímo', 'euros')]
+
+# print("Introduza a palavra:", end='')
+# palavra = input()
+
+# # if palavra[len(palavra)-1] == "l" and palavra[len(palavra)-2] == "a":
+# #     print("Termina em al")
+
+# for singular, plural in lista_de_palavras:
+#     if singular == palavra:
+#         final = plural
+
+# if palavra[-2:] == "al":
 #     print("Termina em al")
+#     final = palavra.replace('al', 'ais')
+# else:
+#     final = palavra + "s"
 
-for singular, plural in lista_de_palavras:
-    if singular == palavra:
-        final = plural
-
-if palavra[-2:] == "al":
-    print("Termina em al")
-    final = palavra.replace('al', 'ais')
-else:
-    final = palavra + "s"
-
-print("O plural de", palavra, " é ", final)
+# print("O plural de", palavra, " é ", final)
